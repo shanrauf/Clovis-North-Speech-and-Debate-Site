5bfa3b39ef183ec1c5ed017651cf2687370a4ef2
Shan Rauf <s.rauf124@gmail.com>
2018-09-27 15:42:08 -0700

    Editting More Info Buttons

--- js/upcomingTournaments.js
+++ js/upcomingTournaments.js
@@ -1,3 +1,5 @@
+/* Home Page Tournaments Pagination */
+
 function tournamentsPagination1() {
     var page1 = document.getElementById("tournamentsPagination1");
     var page2 = document.getElementById("tournamentsPagination2");
@@ -65,4 +67,25 @@ function tournamentsPagination5() {
  	page3.style.display = "none";
  	page4.style.display = "none";
  	page5.style.display = "flex";
+}
+
+/* Tournaments "More Info" Buttons */
+
+function tournamentsButton1() {
+    var row1 = document.getElementById("tournamentsRow1");
+    var row2 = document.getElementById("tournamentsRow2");
+    var row3 = document.getElementById("tournamentsRow3");
+    var row4 = document.getElementById("tournamentsRow4");
+    var row5 = document.getElementById("tournamentsRow5");
+
+    var tourney1 = document.getElementById("tourney1");
+    var tourney2 = document.getElementById("tourney2");
+    var tourney3 = document.getElementById("tourney3");
+    var tourney4 = document.getElementById("tourney4");
+
+    /* Hides All But Four Tournaments */
+    row2.style.display = "none";
+    row3.style.display = "none";
+    row4.style.display = "none";
+    row5.style.display = "none";
 }
