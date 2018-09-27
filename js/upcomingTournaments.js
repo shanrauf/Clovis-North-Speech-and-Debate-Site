b0e78af321746d1290b6368c1c7b0ad343775af3
Shan Rauf <s.rauf124@gmail.com>
2018-09-27 00:27:17 -0700

    Updated Home, Tournaments, etc

--- js/upcomingTournaments.js
+++ js/upcomingTournaments.js
@@ -1,8 +1,68 @@
-function tournamentsPagination() {
-    var x = document.getElementById("tournamentsPage1");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }
+function tournamentsPagination1() {
+    var page1 = document.getElementById("tournamentsPagination1");
+    var page2 = document.getElementById("tournamentsPagination2");
+    var page3 = document.getElementById("tournamentsPagination3");
+    var page4 = document.getElementById("tournamentsPagination4");
+    var page5 = document.getElementById("tournamentsPagination5");
+
+ 	page1.style.display = "flex";
+ 	page2.style.display = "none";
+ 	page3.style.display = "none";
+ 	page4.style.display = "none";
+ 	page5.style.display = "none";   
+}
+function tournamentsPagination2() {
+    var page1 = document.getElementById("tournamentsPagination1");
+    var page2 = document.getElementById("tournamentsPagination2");
+    var page3 = document.getElementById("tournamentsPagination3");
+    var page4 = document.getElementById("tournamentsPagination4");
+    var page5 = document.getElementById("tournamentsPagination5");
+
+ 	page1.style.display = "none";
+ 	page2.style.display = "flex";
+ 	page3.style.display = "none";
+ 	page4.style.display = "none";
+ 	page5.style.display = "none";
+}
+
+function tournamentsPagination3() {
+    var page1 = document.getElementById("tournamentsPagination1");
+    var page2 = document.getElementById("tournamentsPagination2");
+    var page3 = document.getElementById("tournamentsPagination3");
+    var page4 = document.getElementById("tournamentsPagination4");
+    var page5 = document.getElementById("tournamentsPagination5");
+
+ 	page1.style.display = "none";
+ 	page2.style.display = "none";
+ 	page3.style.display = "flex";
+ 	page4.style.display = "none";
+ 	page5.style.display = "none";
+}
+
+function tournamentsPagination4() {
+    var page1 = document.getElementById("tournamentsPagination1");
+    var page2 = document.getElementById("tournamentsPagination2");
+    var page3 = document.getElementById("tournamentsPagination3");
+    var page4 = document.getElementById("tournamentsPagination4");
+    var page5 = document.getElementById("tournamentsPagination5");
+
+ 	page1.style.display = "none";
+ 	page2.style.display = "none";
+ 	page3.style.display = "none";
+ 	page4.style.display = "flex";
+ 	page5.style.display = "none";
+}
+
+function tournamentsPagination5() {
+    var page1 = document.getElementById("tournamentsPagination1");
+    var page2 = document.getElementById("tournamentsPagination2");
+    var page3 = document.getElementById("tournamentsPagination3");
+    var page4 = document.getElementById("tournamentsPagination4");
+    var page5 = document.getElementById("tournamentsPagination5");
+
+ 	page1.style.display = "none";
+ 	page2.style.display = "none";
+ 	page3.style.display = "none";
+ 	page4.style.display = "none";
+ 	page5.style.display = "flex";
 }
