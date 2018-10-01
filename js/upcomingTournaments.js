5a616d53bae3b8e45b9199c4cb1ec42c3849665b
Shan Rauf <s.rauf124@gmail.com>
2018-10-01 13:37:06 -0700

    Improved More Info button

--- js/upcomingTournaments.js
+++ js/upcomingTournaments.js
@@ -72,20 +72,45 @@ function tournamentsPagination5() {
 /* Tournaments "More Info" Buttons */
 
 function tournamentsButton1() {
+    /* Storing every row to use below */
     var row1 = document.getElementById("tournamentsRow1");
     var row2 = document.getElementById("tournamentsRow2");
     var row3 = document.getElementById("tournamentsRow3");
     var row4 = document.getElementById("tournamentsRow4");
     var row5 = document.getElementById("tournamentsRow5");
 
-    var tourney1 = document.getElementById("tourney1");
-    var tourney2 = document.getElementById("tourney2");
-    var tourney3 = document.getElementById("tourney3");
-    var tourney4 = document.getElementById("tourney4");
-
     /* Hides All But Four Tournaments */
     row2.style.display = "none";
     row3.style.display = "none";
     row4.style.display = "none";
     row5.style.display = "none";
+
+    /* Storing every card in the remaining row to use below */
+    var tourney1 = document.getElementById("tourney1");
+    var tourney2 = document.getElementById("tourney2");
+    var tourney3 = document.getElementById("tourney3");
+    var tourney4 = document.getElementById("tourney4");
+
+    /* Gets Rid of Other 3 Tourneys in the Row */
+    tourney2.style.display = "none";
+    tourney3.style.display = "none";
+    tourney4.style.display = "none";
+
+    /* Expands card to parent div */
+    tourney1.classList.remove("col-md-4");
+    tourney1.classList.remove("col-sm-6");
+    tourney1.classList.remove("portfolio-item");
+    tourney1.classList.remove("col-lg-3");
+    row1.classList.remove("row");
+    
+    /* Fix Photo Aspect Ratio */
+    var tournamentImage = document.getElementById("tournamentImage");
+
+    tournamentImage.style.height = "50%";
+    tournamentImage.style.width = "30%";
+    tournamentImage.style.display = "block";
+    tournamentImage.style.margin = "0 auto";
+    tournamentImage.style.maxwidth = "100%";
+    tournamentImage.style.height = "auto";
+
 }
